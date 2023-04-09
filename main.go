80ee55cceed9e0c41246a14fe4c35fbe63e4e845
Akecel <axel@bailfacile.fr>
2023-04-09 13:44:33 +0200

    ✨ Update

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	// Create an instance of the app structure
@@ -22,21 +26,19 @@ func main() {
 		Title:  "Elizabeth",
 		Width:  1024,
 		Height: 768,
-		// MinWidth:          720,
-		// MinHeight:         570,
-		// MaxWidth:          1280,
-		// MaxHeight:         740,
 		DisableResize:     false,
 		Fullscreen:        false,
 		Frameless:         false,
 		StartHidden:       false,
 		HideWindowOnClose: false,
-		BackgroundColour:  &options.RGBA{255, 255, 255, 255},
+		BackgroundColour:  &options.RGBA{17, 18, 25, 255},
 		Assets:            assets,
 		LogLevel:          logger.DEBUG,
 		OnStartup:         app.startup,
 		OnDomReady:        app.domReady,
 		OnShutdown:        app.shutdown,
+		CSSDragProperty:   "--wails-draggable",
+        CSSDragValue:      "drag",
 		Bind: []interface{}{
 			app,
 		},
@@ -46,6 +48,25 @@ func main() {
 			WindowIsTranslucent:  false,
 			DisableWindowIcon:    false,
 		},
+		// MacOs platform specific options
+		Mac: &mac.Options{
+            TitleBar: &mac.TitleBar{
+                TitlebarAppearsTransparent: true,
+                HideTitle:                  false,
+                HideTitleBar:               false,
+                FullSizeContent:            false,
+                UseToolbar:                 false,
+                HideToolbarSeparator:       true,
+            },
+            Appearance:           mac.NSAppearanceNameDarkAqua,
+            WebviewIsTransparent: true,
+            WindowIsTranslucent:  false,
+            About: &mac.AboutInfo{
+                Title:   "Elizabeth",
+                Message: "© 2023 Akecel",
+                Icon:    icon,
+            },
+        },
 	})
 
 	if err != nil {
